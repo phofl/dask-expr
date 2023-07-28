fbe396dbcdac90919ed0bf15ae9a9ae9564c7550
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-07-28 11:43:42 +0200

    Add delete files

--- dask_expr/io/iceberg.py
+++ dask_expr/io/iceberg.py
@@ -1,10 +1,18 @@
 import functools
 from typing import Callable
 
+import pyarrow.compute as pc
+import pyarrow.dataset as ds
 from pyarrow._fs import PyFileSystem
 from pyarrow.fs import FSSpecHandler
 from pyiceberg.expressions.visitors import bind, extract_field_ids
-from pyiceberg.io.pyarrow import PyArrowFileIO, _task_to_table, schema_to_pyarrow
+from pyiceberg.io.pyarrow import (
+    ONE_MEGABYTE,
+    PyArrowFileIO,
+    _construct_fragment,
+    _task_to_table,
+    schema_to_pyarrow,
+)
 from pyiceberg.types import ListType, MapType
 
 from dask_expr._expr import PartitionsFiltered, Projection
@@ -63,7 +71,29 @@ class FromIceberg(PartitionsFiltered, BlockwiseIO):
         return len(self.iceberg_tasks)
 
     def tasks_to_dataframe(self, task):
-        return self._scan_method(task=task).to_pandas()
+        delete_files = task.delete_files
+        chunked_delete_vectors = []
+
+        for delete_file in delete_files:
+            delete_fragment = _construct_fragment(
+                self.fs,
+                delete_file,
+                file_format_kwargs={
+                    "dictionary_columns": ("file_path",),
+                    "pre_buffer": True,
+                    "buffer_size": ONE_MEGABYTE,
+                },
+            )
+            table = ds.Scanner.from_fragment(
+                fragment=delete_fragment,
+                filter=pc.field("file_path") == task.file.file_path,
+                columns=["pos"],
+            ).to_table()
+            chunked_delete_vectors.append(table["pos"])
+
+        return self._scan_method(
+            task=task, positional_deletes=chunked_delete_vectors
+        ).to_pandas()
 
     @functools.cached_property
     def _scan_method(self) -> Callable:
@@ -84,7 +114,6 @@ class FromIceberg(PartitionsFiltered, BlockwiseIO):
             bound_row_filter=bound_row_filter,
             projected_schema=self.projected_schema,
             projected_field_ids=projected_field_ids,
-            positional_deletes=None,
             case_sensitive=self.table_scan.case_sensitive,
             rows_counter=None,
             limit=None,
